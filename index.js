ad75578a148ac01da88f233c999e02db35385978
Rajas Paranjpe <paranjperajas@gmail.com>
2021-05-01 12:28:57 -0500

    feat(indent): Indents of 2 spaces

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ export const lib = () => 'noice'
 export const good = () => 3
 
 const kindMessages = [
-    'Thank You',
-    'You are amazing',
-    'Good',
-    'Nice',
-    'You\'re Welcome',
-    'Hello',
-    'Hi',
-    'Good morning',
-    'Thanks',
-    'Great',
-    'Wonderful',
-    'Good afternoon',
-    'Good evening',
-    'Good night',
-    'You\'re nice',
-    'Good job',
-    'You are awesome',
-    'You are good',
-    'You are'
+  'Thank You',
+  'You are amazing',
+  'Good',
+  'Nice',
+  'You\'re Welcome',
+  'Hello',
+  'Hi',
+  'Good morning',
+  'Thanks',
+  'Great',
+  'Wonderful',
+  'Good afternoon',
+  'Good evening',
+  'Good night',
+  'You\'re nice',
+  'Good job',
+  'You are awesome',
+  'You are good',
+  'You are'
 ]
 /**
  * Returns a random kind message.
@@ -46,5 +46,5 @@ export const kind = () => randomItem(kindMessages)
  * Randomly returns either `'head'` or `'tail'`, like a coin flip.
  */
 export const coin = () => Math.random() < 0.5
-    ? 'head'
-    : 'tail'
+  ? 'head'
+  : 'tail'
